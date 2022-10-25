f79ab26cc22b8154275b450e3768f56c1de560d9
Kouroshezz <kourosh.ezz@gmail.com>
2022-10-25 16:05:17 +0330

    fixing banner flex overflow

--- src/Pages/Homepage/banner.js
+++ src/Pages/Homepage/banner.js
@@ -4,7 +4,7 @@ function BannerHomepage({ title, image, subTitle }) {
     return (
         <div className='homepage-banners position-relative rounded-5 my-5 my-lg-1'>
             <img src={image} className='img-fluid' />
-            <div className="content d-flex flex-column justify-content-center">
+            <div className="content d-flex flex-column justify-content-center w-50">
                 <h4>{title}</h4>
                 <p className='mt-5'>{subTitle}</p>
             </div>
