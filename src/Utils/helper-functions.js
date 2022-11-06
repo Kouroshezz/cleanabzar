c9e67b339c28471c09b1aa805e8cf6513dbfb981
Kouroshezz <kourosh.ezz@gmail.com>
2022-11-06 11:04:45 +0330

    adding space and dash to farsi regex pattern

--- src/Utils/helper-functions.js
+++ src/Utils/helper-functions.js
@@ -1,5 +1,5 @@
 // =========  Farsi Characters unicode
-export const persianLetterPattern = /^[\u0621-\u0628\u062A-\u063A\u0641-\u0642\u0644-\u0648\u064E-\u0651\u0655\u067E\u0686\u0698\u06A9\u06AF\u06BE\u06CC]+$/;
+export const persianLetterPattern = /^[\u0621-\u0628\u062A-\u063A\u0641-\u0642\u0644-\u0648\u064E-\u0651\u0655\u067E\u0686\u0698\u06A9\u06AF\u06BE\u06CC_ -]+$/;
 // ========== check national code function ============ 
 export function checkNC(meli_code) {
     if (meli_code.length == 10) {
