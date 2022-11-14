103abfe0f221f08d074014b998ff565db9f8822c
Kouroshezz <kourosh.ezz@gmail.com>
2022-11-14 10:00:15 +0330

    filter component

--- src/Components/Products/filter.js
+++ src/Components/Products/filter.js
@@ -3,11 +3,11 @@ import * as noUiSlider from 'nouislider';
 import 'nouislider/dist/nouislider.css';
 import wNumb from 'wnumb';
 
-function Filter() {
+function Filter({ setPrice }) {
 
+  const minPrice = useRef(null);
+  const maxPrice = useRef(null);
   const sliderRef = useRef(null);
-  const minPrice = useRef(null)
-  const maxPrice = useRef(null)
 
   useEffect(() => {
     noUiSlider?.create(sliderRef?.current, {
@@ -30,7 +30,7 @@ function Filter() {
     });
 
     // sliderRef.current.noUiSlider.on('end', (e) => console.log(e))
-    sliderRef.current.noUiSlider.on('change', (e) => console.log('Price has updated'))
+    sliderRef.current.noUiSlider.on('change', (e) => setPrice({ min: e[0], max: e[1] }))
     sliderRef.current.noUiSlider.on('slide', (e) => {
       minPrice.current.innerText = `از ${e[0]}`;
       maxPrice.current.innerText = `تا ${e[1]}`;
@@ -39,7 +39,6 @@ function Filter() {
   }, [])
 
   useEffect(() => {
-
     return () => {
       sliderRef?.current?.noUiSlider.off();
       sliderRef?.current?.noUiSlider.destroy();
@@ -48,7 +47,7 @@ function Filter() {
 
   return (
     <div>
-      <div ref={sliderRef} className='mt-4' onChange={(e) => console.log(e)}></div>
+      <div ref={sliderRef} className='mt-4' ></div>
       <div className='d-flex justify-content-between text-14 text-gray mt-4'>
         <span ref={minPrice}>{Number(500000).toLocaleString()} تومان </span>
         <span ref={maxPrice}>{Number(10000000).toLocaleString()}تومان </span>
