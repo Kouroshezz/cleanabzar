135188102b3f52e9b5f78d6fcd9857e1f159499b
kouroshezz <kourosh.ezz@gmail.com>
2023-01-09 12:42:27 +0330

    modal's background get animated

--- src/Components/Modal/index.js
+++ src/Components/Modal/index.js
@@ -16,7 +16,7 @@ function Modal(props) {
 
     return (
         <>
-            <div className={`${styles.wrapper}`} onClick={() => close()}> </div>
+            <div className={`${styles.wrapper}  animate__animated animate__fadeIn animate__faster`} onClick={() => close()}> </div>
             <div className={`${styles.content} animate__animated animate__fadeIn animate__faster`}>
                 <div className="d-flex align-items-center justify-content-between border-b pb-3 mb-4">
                     <div>
